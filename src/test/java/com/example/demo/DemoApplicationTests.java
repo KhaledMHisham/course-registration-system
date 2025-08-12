f3b18434be639598b9d110dbdae8176d007a8fb0
mohamed-eldafrawy <mohamed.eldafrawy@vodafone.com>
2025-08-12 13:47:13 +0200

    Delete StudentServiceTest.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.entity.Course;
+import com.example.demo.service.CourseService;
+import java.util.Collection;
+import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	CourseService courseService;
+
 	@Test
 	void contextLoads() {
+		Course course = new Course();
+		course.setTitle("test course");
+		course.setDescription("test course description");
+
+		Course savedCourse = courseService.save(course);
+		Collection<Course> all = courseService.findAll();
+
+		Assertions.assertNotNull(savedCourse);
+		Assertions.assertEquals(course.getTitle(), savedCourse.getTitle());
+
+		Assertions.assertNotNull(all);
+		Assertions.assertEquals(1,all.size());
 	}
 
 }
